d66bc6fba45332e37224cf26694aaff34b4c8ed4
pawel <pawel.langwerski@gmail.com>
2017-08-27 19:55:31 +0200

    Initial commit

---
+++ src/main/java/com/fleet/manager/FleetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.fleet.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagerApplication.class, args);
+
+	}
+}
