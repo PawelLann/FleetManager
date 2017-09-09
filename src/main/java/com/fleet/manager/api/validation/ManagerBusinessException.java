2ce06748c5f9e15b070f37b4449b8e4384983959
Pawel Lann <pawel.langwerski@coi.gov.pl>
2017-09-09 19:20:35 +0200

    Add classes for exceptions and exception messages

---
+++ src/main/java/com/fleet/manager/api/validation/ManagerBusinessException.java
@@ -0,0 +1,17 @@
+package com.fleet.manager.api.validation;
+
+import lombok.Getter;
+
+/**
+ * Created by pawel.langwerski@coi.gov.pl on 09.09.17.
+ */
+@Getter
+public class ManagerBusinessException extends RuntimeException {
+
+  private final ExceptionMessage exceptionMessage;
+
+  public ManagerBusinessException(ExceptionMessage exceptionMessage){
+    super(exceptionMessage.getMessage());
+    this.exceptionMessage = exceptionMessage;
+  }
+}
